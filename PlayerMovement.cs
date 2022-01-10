51768a735ee5d3d461b8f08d0f764d4cbfb35924
Whatshon <94352063+Whatshon@users.noreply.github.com>
2022-01-11 01:25:13 +0700

    Update PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,3 +1,43 @@
-﻿internal class PlayerMovement
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private CharacterController controller;
+    private float speed = 12f;
+
+    private float gravity = -25f;
+    [SerializeField] private Transform grounded;
+    private float groundedDistance = 0.4f;
+    [SerializeField]private LayerMask groundedMask;
+    private float jumpHeight = 3f;
+
+    private Vector3 velocity;
+    bool isGrounded;
+
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(grounded.position, groundedDistance, groundedMask);
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * y;
+
+        controller.Move(move * speed *Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump")&& isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
 }
